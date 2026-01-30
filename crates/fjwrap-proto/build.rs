5001dd76b3cffbf05870454abe9b15bd96bc9b33
RyeBread <rye@rye.moe>
2026-01-29 20:31:24 -0700

    Split proto files

--- crates/fjwrap-proto/build.rs
+++ crates/fjwrap-proto/build.rs
@@ -3,6 +3,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_server(true)
         .build_client(true)
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
-        .compile_protos(&["proto/fjwrap.proto"], &["proto/"])?;
+        .compile_protos(
+            &["proto/kv.proto", "proto/cluster.proto", "proto/raft.proto"],
+            &["proto/"],
+        )?;
     Ok(())
 }
